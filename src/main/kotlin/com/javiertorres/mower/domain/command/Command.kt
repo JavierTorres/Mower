3faaf5d38cd82ae35f86c44acb34485b92199f46
javier.bracero <javier.bracero@adevinta.com>
2020-09-13 15:32:23 +0200

    fixed for the corresponding parameter in the supertype 'Command' for 'newPosition'

--- src/main/kotlin/com/javiertorres/mower/domain/command/Command.kt
+++ src/main/kotlin/com/javiertorres/mower/domain/command/Command.kt
@@ -3,5 +3,5 @@ package com.javiertorres.mower.domain.command
 import com.javiertorres.mower.domain.location.Position
 
 interface Command {
-    fun execute(position: Position): Position
+    fun execute(newPosition: Position): Position
 }
